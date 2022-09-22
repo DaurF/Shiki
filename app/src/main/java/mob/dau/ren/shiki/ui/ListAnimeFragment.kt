47a17320b8942fec443ed962412464f3591d933e
DaurF <213053@astanait.edu.kz>
2022-09-23 00:02:23 +0600

    First commit

---
+++ app/src/main/java/mob/dau/ren/shiki/ui/ListAnimeFragment.kt
@@ -0,0 +1,31 @@
+package mob.dau.ren.shiki.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mob.dau.ren.shiki.R
+import mob.dau.ren.shiki.databinding.FragmentListAnimeBinding
+
+class ListAnimeFragment : Fragment() {
+    private var _binding: FragmentListAnimeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListAnimeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun bind() {
+
+    }
+}
